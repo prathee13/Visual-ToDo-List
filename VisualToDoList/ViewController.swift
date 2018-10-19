ea4eb59fa531908ff591c24ea977ed2e3c95c066
Pratheeksha Ravindra Naik <prp412@udml9.local>
2018-10-19 10:14:09 -0600

    Initial Commit

---
+++ VisualToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisualToDoList
+//
+//  Created by Pratheeksha Ravindra Naik on 2018-10-19.
+//  Copyright © 2018 com/uregina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
