23ce283d4738b095e64123a0411a6f31d135d7c8
Pratheeksha Ravindra Naik <prp412@udml9.local>
2018-10-22 14:41:24 -0600

    Set Default Text

--- VisualToDoList/ViewController.swift
+++ VisualToDoList/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var toDoLabel: UILabel!
+    
+    //MARK: Actions
+    
+    
+    
+    @IBAction func Button(_ sender: UIButton) {
+        toDoLabel.text = "Default Text"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
